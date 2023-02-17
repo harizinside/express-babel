36cadfee991f0ac96f1da1133ca39da0d5a5157b
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2023-02-17 09:28:17 +0700

    add cors and minify
    for better performance

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import indexRouter from '@routes/index';
 import usersRouter from '@routes/users';
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
